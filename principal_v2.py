432ec852b7215b8f91fe2c5d0df61d1881334530
juanhahn <juanhahn@gmail.com>
2018-06-26 02:01:05 -0400

    Update principal_v2.py

--- principal_v2.py
+++ principal_v2.py
@@ -12,7 +12,7 @@ import face_identificaction as fid
 if __name__ == "__main__":
     institution_bucket = 'instituciondiegoportales'
     class_name = 'tic3_1'
-    path = './instituciondiegoportales_test/'
+    path = './'+institution_bucket+'_test/'
     valid_images = [".jpg"]
     file_list = os.listdir(path)
     for photo_path in file_list:
@@ -20,5 +20,8 @@ if __name__ == "__main__":
         if ext.lower() not in valid_images:
             continue
         fid.verify_face(institution_bucket, class_name,
-                        Image.open(os.path.join(path, photo_path)))
-        os.remove(os.path.join(path, photo_path))
+                        Image.open(path+photo_path)
+        #Error sucede en la linea 90 de atm: dynamoDB no tiene un atributo ni funcion llamado "Table"
+        os.remove(path+photo_path)
+                        #Image.open(os.path.join(path, photo_path)))
+        #os.remove(os.path.join(path, photo_path)) Estos dos me estaban dando error antes, pero los guarde por si acaso
