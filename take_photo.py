c0a155c5c113fe2a574780e4cb665e49b4ffbd28
GGonzalezRojas <gustavo.gonzalez.r@hotmail.com>
2018-06-26 17:45:32 -0400

    Cambios

---
+++ take_photo.py
@@ -0,0 +1,8 @@
+from picamera import PiCamera
+from time import sleep
+
+
+camera = PiCamera
+institution_bucket = 'instituciondiegoportales'
+path = './'+institution_bucket+'_test/'
+camera.capture(path)
