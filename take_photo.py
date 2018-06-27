ea49fc1f12e905dea4a6f099993acf82ed06a0fa
GGonzalez <gustavo.gonzalez.r@hotmail.com>
2018-06-27 15:08:06 +0000

    Cambios varios

--- take_photo.py
+++ take_photo.py
@@ -1,12 +1,15 @@
 from picamera import PiCamera
 from time import sleep
-camera = PiCamera
+camera = PiCamera()
 institution_bucket = 'instituciondiegoportales'
 path = './'+institution_bucket+'_test/'
+path_v2 = './instituciondiegoportales_test/image%s.jpg'
 i = 0
 while True:
     file_path = path + 'image{}.jpg'.format(i)
+    print(file_path)
+    sleep(5)
     camera.capture(file_path)
     i += 1
-    sleep(5)
+    
 
