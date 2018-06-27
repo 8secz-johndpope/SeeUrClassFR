57774826a05d7946931b0a12c0c27a7570e97fdd
GGonzalezRojas <gustavo.gonzalez.r@hotmail.com>
2018-06-27 19:26:07 -0400

    Cambios take distance

--- take_distance.py
+++ take_distance.py
@@ -2,39 +2,22 @@
 import time
 import RPi.GPIO as GPIO
 
-
-# Use board based pin numbering
 GPIO.setmode(GPIO.BOARD)
-
-
 def ReadDistance(pin):
-    print("primera entrada")
     GPIO.setup(pin, GPIO.OUT)
     GPIO.output(pin, 0)
-
     time.sleep(0.000002)
-
-    #send trigger signal
     GPIO.output(pin, 1)
-
     time.sleep(0.000005)
-    print("segunda entrada")
     GPIO.output(pin, 0)
-
     GPIO.setup(pin, GPIO.IN)
-
     while GPIO.input(pin) == 0:
         starttime = time.time()
-        print("quinta entrada")    
-    print("tercera entrada")
     while GPIO.input(pin) == 1:
         endtime = time.time()
-        print("sexta entrada")
-
     duration = endtime-starttime
     # Distance is defined as time/2 (there and back) * speed of sound 34000 cm/s
     distance = duration*34000/2
-    print("septima entrada")
     return distance
 while True:
    distance = ReadDistance(11)
